dfd2d5f61e5b806e60fbb3b5f3a1217cd1de0953
B. Petersen <r10s@b44t.com>
2017-10-08 21:00:44 +0200

    Correct return value when exporting keys.

--- src/mrmailbox_imex.c
+++ src/mrmailbox_imex.c
@@ -327,6 +327,7 @@ static void export_key_to_asc_file(mrmailbox_t* mailbox, const char* dir, int id
 
 static int export_self_keys(mrmailbox_t* mailbox, const char* dir)
 {
+	int           success = 0;
 	sqlite3_stmt* stmt = NULL;
 	int           id = 0, is_default = 0;
 	mrkey_t*      public_key = mrkey_new();
@@ -349,12 +350,14 @@ static int export_self_keys(mrmailbox_t* mailbox, const char* dir)
 			export_key_to_asc_file(mailbox, dir, id, private_key, is_default);
 		}
 
+		success = 1;
+
 cleanup:
 	if( locked ) { mrsqlite3_unlock(mailbox->m_sql); }
 	if( stmt ) { sqlite3_finalize(stmt); }
 	mrkey_unref(public_key);
 	mrkey_unref(private_key);
-	return 0;
+	return success;
 }
 
 
