c7a22234500006a97ddb36da3c7821f3787c4685
B. Petersen <r10s@b44t.com>
2017-11-19 11:42:02 +0100

    Doxygen

--- docs/html/search/classes_0.js
+++ docs/html/search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['mrarray_5ft',['mrarray_t',['../structmrarray__t.html',1,'']]],
   ['mrchat_5ft',['mrchat_t',['../structmrchat__t.html',1,'']]],
   ['mrchatlist_5ft',['mrchatlist_t',['../structmrchatlist__t.html',1,'']]],
   ['mrcontact_5ft',['mrcontact_t',['../structmrcontact__t.html',1,'']]],
