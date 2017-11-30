cd9a1ee5b97ba359eb99bbe42f3676abaa04cec5
B. Petersen <r10s@b44t.com>
2017-11-30 14:06:44 +0100

    doxygen

--- docs/html/search/variables_0.js
+++ docs/html/search/variables_0.js
@@ -1,18 +1,13 @@
 var searchData=
 [
-  ['m_5farchived',['m_archived',['../structmrchat__t.html#a5b28b343ceb290d4a613d0e5a7d528bc',1,'mrchat_t']]],
   ['m_5fchat_5fid',['m_chat_id',['../structmrmsg__t.html#ac19bdc40e452c8997dd58474a7be6880',1,'mrmsg_t']]],
-  ['m_5fdraft_5ftext',['m_draft_text',['../structmrchat__t.html#abe916f48ac83c2ca6476312eae084cfa',1,'mrchat_t']]],
-  ['m_5fdraft_5ftimestamp',['m_draft_timestamp',['../structmrchat__t.html#a565101f53278f5ab1ff58317ffc9555c',1,'mrchat_t']]],
   ['m_5ffrom_5fid',['m_from_id',['../structmrmsg__t.html#a299981cb89b25ebaaedf7a8486a86ee6',1,'mrmsg_t']]],
   ['m_5fid',['m_id',['../structmrchat__t.html#a1591431716413350eaffce0b588e4911',1,'mrchat_t::m_id()'],['../structmrcontact__t.html#ab2ee31297c327e04ed14673adbd49716',1,'mrcontact_t::m_id()'],['../structmrmsg__t.html#a793898c8da24c717dc2bb1d91faac801',1,'mrmsg_t::m_id()']]],
-  ['m_5fname',['m_name',['../structmrchat__t.html#abbd344d8d361a1a4d14bfc9b5dc57140',1,'mrchat_t']]],
   ['m_5fstate',['m_state',['../structmrlot__t.html#acc3e27731b8dbe46733cab6001032f9a',1,'mrlot_t']]],
   ['m_5ftext1',['m_text1',['../structmrlot__t.html#afa71b7a11147bc6e73313986a08fdcd7',1,'mrlot_t']]],
   ['m_5ftext1_5fmeaning',['m_text1_meaning',['../structmrlot__t.html#a38067382d835f912ad1d4f7350306dc0',1,'mrlot_t']]],
   ['m_5ftext2',['m_text2',['../structmrlot__t.html#ad894f96fe9a1672808944fdb0ba04628',1,'mrlot_t']]],
   ['m_5ftimestamp',['m_timestamp',['../structmrlot__t.html#af71c6758491ef50b7c25a11c0d9fee17',1,'mrlot_t']]],
   ['m_5fto_5fid',['m_to_id',['../structmrmsg__t.html#ad549fc02587ba284fda2bbecd162b16a',1,'mrmsg_t']]],
-  ['m_5ftype',['m_type',['../structmrchat__t.html#a568c4e6fb9633ffe9a76428db16e0201',1,'mrchat_t']]],
   ['m_5fuserdata',['m_userdata',['../structmrmailbox__t.html#a6c557153209e128b69301246dbf9e230',1,'mrmailbox_t']]]
 ];
