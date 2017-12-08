04d26488a1ac8cd0d6daced9a865d13f34c18c7c
B. Petersen <r10s@b44t.com>
2017-12-08 19:11:44 +0100

    use the most outer protected header - this is typically created in sync with the normal, unprotected header

--- src/mrmimeparser.c
+++ src/mrmimeparser.c
@@ -1071,13 +1071,12 @@ static int mrmimeparser_parse_mime_recursive(mrmimeparser_t* ths, struct mailmim
 			return 0;
 		}
 
-		size_t dummy = 0;
-		struct mailimf_fields* temp_fields = NULL;
-		if( mailimf_envelope_and_optional_fields_parse(mime->mm_mime_start, mime->mm_length, &dummy, &temp_fields)==MAILIMF_NO_ERROR // what is the difference between mailimf_envelope_and_optional_fields_parse() mailimf_fields_parse()
-		 && temp_fields!=NULL ) {
-			if( ths->m_header_protected ) { mailimf_fields_free(ths->m_header_protected); } /* use the most inner protected header */
-			ths->m_header_protected = temp_fields; /* freed later using mailimf_fields_free() */
-			mrmailbox_log_info(ths->m_mailbox, 0, "Protected headers found in MIME header: Will be used.");
+		if( ths->m_header_protected==NULL ) { /* use the most outer protected header - this is typically created in sync with the normal, unprotected header */
+			size_t dummy = 0;
+			if( mailimf_envelope_and_optional_fields_parse(mime->mm_mime_start, mime->mm_length, &dummy, &ths->m_header_protected)==MAILIMF_NO_ERROR // what is the difference between mailimf_envelope_and_optional_fields_parse() mailimf_fields_parse()
+			 && ths->m_header_protected!=NULL ) {
+				mrmailbox_log_info(ths->m_mailbox, 0, "Protected headers found in MIME header: Will be used.");
+			}
 		}
 	}
 
