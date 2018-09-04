c9691e80cb7482ec0f1cf6de06d59e4e9266f1d7
B. Petersen <r10s@b44t.com>
2018-09-04 17:32:37 +0200

    meta: replace the title 'Modules' by 'Constants' in the generated documentation

--- docs/update.sh
+++ docs/update.sh
@@ -4,6 +4,26 @@
 UPLOADDIR="../../api"
 
 doxygen
+
+
+# HACK: we use the doxygen commands @defgroup and @addtogroup to sort #define's,
+# however,  doxygen files them under the menu entry "Modules".
+# with the following lines, we correct the menu entry title.
+# if anyone sees a more elegant solution for this, please change it :)
+replace_in_html() {
+	for currfile in *.js; do
+		sed -i "s/$1/$2/g" $currfile
+	done
+	for currfile in *.html; do
+		sed -i "s/$1/$2/g" $currfile
+	done
+}
+cd html
+replace_in_html "Modules" "Constants"
+replace_in_html "all modules" "all constants"
+cd ..
+
+
 mkdir -p ${UPLOADDIR}/docs/
 cp -r html/* ${UPLOADDIR}/docs/
 read -p "if not errors are printed above, press ENTER to commit and push the changes"
@@ -16,6 +36,5 @@ git commit -am "update docs"
 git push
 
 popd > /dev/null
-
 
 
