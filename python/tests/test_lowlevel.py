75153289efb3249d72bea0e109b5e6a30588a3c4
holger krekel <holger@merlinux.eu>
2018-09-09 13:14:29 +0200

    add DC_EVENT_SMTP_CONNECTED to macro

--- python/tests/test_lowlevel.py
+++ python/tests/test_lowlevel.py
@@ -10,3 +10,12 @@ def test_empty_context():
 def test_event_defines():
     assert capi.lib.DC_EVENT_INFO == 100
     assert capi.lib.DC_CONTACT_ID_SELF
+
+def test_sig():
+    sig = capi.lib.dc_get_event_signature_types
+    assert sig(capi.lib.DC_EVENT_INFO) == 2
+    assert sig(capi.lib.DC_EVENT_WARNING) == 2
+    assert sig(capi.lib.DC_EVENT_ERROR) == 2
+    assert sig(capi.lib.DC_EVENT_SMTP_CONNECTED) == 2
+    assert sig(capi.lib.DC_EVENT_IMAP_CONNECTED) == 2
+    assert sig(capi.lib.DC_EVENT_SMTP_MESSAGE_SENT) == 2
