20c12929d30b8709d68577a674683c679dfef5bc
B. Petersen <r10s@b44t.com>
2018-11-12 21:39:46 +0100

    adjust backoff MULTIPLY

--- src/dc_job.c
+++ src/dc_job.c
@@ -412,8 +412,8 @@ static void dc_suspend_smtp_thread(dc_context_t* context, int suspend)
 
 static time_t get_backoff_time_offset(int c_tries)
 {
-	#define MULTIPLY 5
-	#define JOB_RETRIES 16 // results in max. 17 hours
+	#define MULTIPLY 60
+	#define JOB_RETRIES 16 // results in ~3 weeks for the last backoff timespan
 
 	int N = (int)pow((double)2, c_tries) - 1;
 
