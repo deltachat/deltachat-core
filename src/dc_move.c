6898ca920288476f3256d3a6a03d8e600d80d46d
B. Petersen <r10s@b44t.com>
2018-12-21 17:07:35 +0100

    implement determine_next_move_state
    fix tabs

--- src/dc_move.c
+++ src/dc_move.c
@@ -26,55 +26,113 @@ dc_move_state_t dc_resolve_move_state(dc_context_t* context, const dc_msg_t* msg
 }
 
 
-// Return the next move state for this message.
-// Only call this function if the message is pending.
-// This function works with the DB, does not perform any IMAP commands.
+static int is_moved_message(dc_msg_t* msg)
+{
+	// TODO: check if comparing against the current folder is sufficient.
+	// the python implementation differs between the foldername
+	// and the target_foldername.
+	// we currently do not have this distinction,
+	// server_folder is just the folder where the message is currently in;
+	// it is updated after the move-job.
+
+	if (dc_is_mvbox(msg->context, msg->server_folder)) {
+		return 1;
+	}
+
+	return 0;
+}
+
+
 dc_move_state_t dc_determine_next_move_state(dc_context_t* context, const dc_msg_t* msg)
 {
+	// Return the next move state for this message.
+	// Only call this function if the message is pending.
+	// This function works with the DB, does not perform any IMAP commands.
 	dc_move_state_t res = DC_MOVE_STATE_UNDEFINED;
-	int             last_dc_count = 0;
+	dc_msg_t*       newmsg = NULL;
 
-	if (msg==NULL || msg->move_state!=DC_MOVE_STATE_PENDING) {
+	if (context==NULL || msg==NULL) {
 		goto cleanup;
 	}
 
+	// TODO: where is a good place to check for user-disabled moving?
 	if (dc_sqlite3_get_config_int(context->sql, "mvbox_move", DC_MVBOX_MOVE_DEFAULT)==0) {
 		goto cleanup;
 	}
 
+	dc_log_info(context, 0, "[move] shall_move %s", msg->rfc724_mid);
+
 	if (!dc_is_inbox(context, msg->server_folder)
 	 && !dc_is_sentbox(context, msg->server_folder)) {
+		dc_log_info(context, 0, "[move] %s neither in INBOX nor in SENTBOX", msg->rfc724_mid);
+		goto cleanup;
+	}
+
+	if (msg->move_state!=DC_MOVE_STATE_PENDING) {
+		dc_log_info(context, 0, "[move] %s is not PENDING, this should not happen", msg->rfc724_mid);
 		goto cleanup;
 	}
 
 	if (dc_is_mvbox(context, msg->server_folder)) {
+		dc_log_info(context, 0, "[move] %s is already in mvbox, next state is STAY", msg->rfc724_mid);
 		res = DC_MOVE_STATE_STAY;
 		goto cleanup;
 	}
 
-	// having a message in SENTBOX or INBOX that is PENDING and moving is enabled
-
-	#if 0 // to be continued ...
+	// TODO: we should implement some protection against deadlocks here
+	int last_dc_count = 0;
 	while (1)
 	{
 		last_dc_count = msg->is_dc_message? (last_dc_count + 1) : 0;
 
 		if (msg->in_reply_to==NULL || msg->in_reply_to[0]==0)
 		{
-            if (last_dc_count > 0) {
-				return DC_MOVE_STATE_MOVING;
-            }
-            else {
-				return DC_MOVE_STATE_STAY:
-            }
+			dc_log_info(context, 0, "[move] detected thread-start %s message", last_dc_count? "DC" : "CLEAR");
+			if (last_dc_count > 0) {
+				res = DC_MOVE_STATE_MOVING;
+				goto cleanup;
+			}
+			else {
+				res = DC_MOVE_STATE_STAY;
+				goto cleanup;
+			}
 		}
 
-		newmsg
-	}
-	#endif
+		dc_msg_t* temp = dc_msg_new_load(context,
+			dc_rfc724_mid_exists(context, msg->in_reply_to, NULL, NULL));
+		dc_msg_unref(newmsg); // unref after consuming msg->in_reply_to above
+		newmsg = temp;
 
-	res = msg->is_dc_message? DC_MOVE_STATE_MOVING : DC_MOVE_STATE_STAY;
+		if (newmsg==NULL || newmsg->id==0)
+		{
+			dc_log_info(context, 0, "[move] failed to fetch from db: %s", msg->in_reply_to);
+			// we don't have the parent message ... maybe because
+			// it hasn't arrived (yet), was deleted or we failed to
+			// scan/fetch it:
+			if (last_dc_count >= 4) {
+				dc_log_info(context, 0, "[move] no thread start found, bug last 4 messages were dc");
+				res = DC_MOVE_STATE_MOVING;
+				goto cleanup;
+			}
+			else {
+				dc_log_info(context, 0, "[move] pending: missing parent, last_dc_count=%i", last_dc_count);
+				res = DC_MOVE_STATE_PENDING;
+				goto cleanup;
+			}
+		}
+		else if (is_moved_message(newmsg))
+		{
+			dc_log_info(context, 0, "[move] parent was a moved message"); // and papa was rolling stone.
+			res = DC_MOVE_STATE_MOVING;
+			goto cleanup;
+		}
+		else
+		{
+			msg = newmsg;
+		}
+	}
 
 cleanup:
+	dc_msg_unref(newmsg);
 	return res;
 }
