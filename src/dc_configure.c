1e61bcf83c25c820aaf1b2b6764cbb899a07fa0f
B. Petersen <r10s@b44t.com>
2019-02-28 15:51:37 +0100

    explicitly setting the STARTTLS flag

--- src/dc_configure.c
+++ src/dc_configure.c
@@ -1003,7 +1003,9 @@ void dc_job_do_DC_JOB_CONFIGURE_IMAP(dc_context_t* context, dc_job_t* job)
 		if (!dc_smtp_connect(context->smtp, param)) {
 			PROGRESS(860)
 
-			param->send_port = TYPICAL_SMTP_PLAIN_PORT;
+			param->server_flags &= ~DC_LP_SMTP_SOCKET_FLAGS;
+			param->server_flags |=  DC_LP_SMTP_SOCKET_STARTTLS;
+			param->send_port    =   TYPICAL_SMTP_PLAIN_PORT;
 			{ char* r = dc_loginparam_get_readable(param); dc_log_info(context, 0, "Trying: %s", r); free(r); }
 			if (!dc_smtp_connect(context->smtp, param)) {
 				goto cleanup;
