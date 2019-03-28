85352ffd2e3f19a4c91592fc289bbb32a2359d6d
holger krekel <holger@merlinux.eu>
2019-03-28 02:17:47 +0100

    add build_python file

--- .scripts/build_ninja.sh
+++ .scripts/build_ninja.sh
@@ -11,10 +11,3 @@ echo "${NINJA_SHA256}  ninja-linux-${NINJA_VERSION}.zip" | sha256sum -c -
 unzip ninja-linux-${NINJA_VERSION}.zip 
 mv ninja /usr/bin/ninja 
 
-# we use the python3.5 environment as the base environment 
-/opt/python/cp35-cp35m/bin/pip install meson tox
-
-pushd /usr/bin
-
-ln -s /opt/_internal/cpython-3.5.*/bin/meson 
-ln -s /opt/_internal/cpython-3.5.*/bin/tox
