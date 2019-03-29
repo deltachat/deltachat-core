d4aa6628a1c4257f8b3be0584f4f34b5683c7597
holger krekel <holger@merlinux.eu>
2019-03-29 08:14:42 +0100

    c2

--- scripts/doxygen.sh
+++ scripts/doxygen.sh
@@ -2,5 +2,18 @@
 
 set -xe
 
-cd docs
-doxygen 
+
+if [ -n "$DOCS" ] ; then 
+    (cd docs && doxygen)
+    # sync branch docs to py.delta.chat
+    rsync -avz \
+      -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
+      python/doc/_build/html/ \
+      delta@py.delta.chat:build/${BRANCH}
+
+    # Perform the actual deploy to c.delta.chat
+    rsync -avz \
+      -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
+      docs/html/ \
+      delta@py.delta.chat:build-c/${BRANCH}
+fi
--- scripts/travisbuild.sh
+++ scripts/travisbuild.sh
@@ -8,11 +8,11 @@ export BRANCH=${CIRCLE_BRANCH:-test7}
     
 # run doxygen on c-source (needed by later doc-generation steps).
 # XXX modifies the host filesystem docs/xml and docs/html directories
-# which you can then only remove with "sudo rm -rf docs/html docs/xml"
+# XXX which you can then only remove with "sudo rm -rf docs/html docs/xml"
 docker run --rm -it -v $PWD:/mnt -w /mnt/docs deltachat/doxygen doxygen
 
 # run everything else inside docker (TESTS, DOCS, WHEELS) 
-docker run -e DELTA_UPLOAD_SSH_KEY -e BRANCH -e DEVPI_LOGIN -e TESTS -e WHEELS -e DOCS \
+docker run -e BRANCH -e DEVPI_LOGIN -e TESTS -e WHEELS -e DOCS \
            --rm -it -v $(pwd):/mnt -w /mnt \
            $DOCKERIMAGE /mnt/scripts/run_all.sh
 
