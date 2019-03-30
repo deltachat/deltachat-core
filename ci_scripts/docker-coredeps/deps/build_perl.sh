932e5cb4714feff88ad6ad3ba9fc08026054301b
holger krekel <holger@merlinux.eu>
2019-03-30 21:17:48 +0100

    address @flub remarks

--- ci_scripts/docker-coredeps/deps/build_perl.sh
+++ ci_scripts/docker-coredeps/deps/build_perl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 PERL_VERSION=5.28.0
 PERL_SHA256=7e929f64d4cb0e9d1159d4a59fc89394e27fa1f7004d0836ca0d514685406ea8
 curl -O https://www.cpan.org/src/5.0/perl-${PERL_VERSION}.tar.gz
--- ci_scripts/docker-coredeps/deps/build_rust.sh
+++ ci_scripts/docker-coredeps/deps/build_rust.sh
@@ -1,4 +1,5 @@
 
+
 set -e -x 
 
 # Install Rust 
