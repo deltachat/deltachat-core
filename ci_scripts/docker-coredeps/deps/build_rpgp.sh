b62a17b450ad6db71398a412cbd5149310e9da22
holger krekel <holger@merlinux.eu>
2019-03-30 21:29:10 +0100

    slight changes to trigger new build

--- ci_scripts/docker-coredeps/deps/build_rpgp.sh
+++ ci_scripts/docker-coredeps/deps/build_rpgp.sh
@@ -3,6 +3,8 @@
 set -e -x 
 
 # Install RPGP from github 
+# XXX built a particular version like the other dep scripts do
+
 export PATH=$PATH:$HOME/.cargo/bin
 git clone https://github.com/rpgp/rpgp.git 
 cd rpgp/pgp-ffi 
